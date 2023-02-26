34b6dc6448d241680766bfbc7dd86a0b4320fdd8
syontyagi <syontyagi1109@gmail.com>
2023-02-26 12:49:35 -0800

    display updates

--- displaymatrix.c
+++ displaymatrix.c
@@ -1,114 +1,114 @@
 #include "displaymatrix.h"
 
-void  displaybox1(int num)
+void  displaybox1(char num)
 {
 	printf("\033[1;1H");
 	printf("     ");
 	printf("\033[2;1H");
 	printf("    |");  
 	printf("\033[3;1H");
-	printf("  %d |",num);
+	printf("  %c |",num);
 	printf("\033[4;1H");
 	printf(" ___|\n");
 }
 
-void  displaybox2(int num)
+void  displaybox2(char num)
 {
 	printf("\033[1;6H");
 	printf("     ");
 	printf("\033[2;6H");
 	printf("|   |");  
 	printf("\033[3;6H");
-	printf("| %d |",num);
+	printf("| %c |",num);
 	printf("\033[4;6H");
 	printf("|___|\n");
 }
 
-void  displaybox3(int num)
+void  displaybox3(char num)
 {
 	printf("\033[1;11H");
 	printf("     ");
 	printf("\033[2;11H");
 	printf("|    ");  
 	printf("\033[3;11H");
-	printf("| %d  ",num);
+	printf("| %c  ",num);
 	printf("\033[4;11H");
 	printf("|___ \n");
 }
 
-void  displaybox4(int num)
+void  displaybox4(char num)
 {
 	printf("\033[5;1H");
 	printf(" ___\n");
 	printf("\033[6;1H");
 	printf("    |\n");  
 	printf("\033[7;1H");
-	printf("  %d |\n",num);
+	printf("  %c |\n",num);
 	printf("\033[8;1H");
 	printf(" ___|\n");
 }
 
-void  displaybox5(int num)
+void  displaybox5(char num)
 {
 	printf("\033[5;6H");
 	printf(" ___");
 	printf("\033[6;6H");
 	printf("|   |");  
 	printf("\033[7;6H");
-	printf("| %d |",num);
+	printf("| %c |",num);
 	printf("\033[8;6H");
 	printf("|___|\n");
 }
 
-void  displaybox6(int num)
+void  displaybox6(char num)
 {
 	printf("\033[5;11H");
 	printf(" ___ ");
 	printf("\033[6;11H");
 	printf("|    ");
 	printf("\033[7;11H");
-	printf("| %d  ",num);
+	printf("| %c  ",num);
 	printf("\033[8;11H");
 	printf("|___ \n");
 }
 
-void  displaybox7(int num)
+void  displaybox7(char num)
 {
 	printf("\033[9;1H");
 	printf(" ___");
 	printf("\033[10;1H");
 	printf("    |");  
 	printf("\033[11;1H");
-	printf("  %d |",num);
+	printf("  %c |",num);
 	printf("\033[12;1H");
 	printf("    |");
 }
 
-void  displaybox8(int num)
+void  displaybox8(char num)
 {
 	printf("\033[9;6H");
 	printf(" ___");
 	printf("\033[10;6H");
 	printf("|   |");  
 	printf("\033[11;6H");
-	printf("| %d |",num);
+	printf("| %c |",num);
 	printf("\033[12;6H");
 	printf("|   |\n");
 }
 
-void  displaybox9(int num)
+void  displaybox9(char num)
 {
 	printf("\033[9;11H");
 	printf(" ___");
 	printf("\033[10;11H");
 	printf("|    ");  
 	printf("\033[11;11H");
-	printf("| %d  ",num);
+	printf("| %c  ",num);
 	printf("\033[12;11H");
 	printf("|    \n");
 }
 
-int displaybox(int pos, int num)
+int displaybox(int pos, char num)
 {
 	switch (pos) {
 		case 1:
--- syonmain.c
+++ syonmain.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
 	//int num=3, pos=9;
 	int len =9;
-	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int arr[] = {49, 50, 51, 52, 53, 54, 55, 56, 57};
 	displayallbox(arr, len);
 
 	return 0;
