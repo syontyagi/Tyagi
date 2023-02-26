6470052690df43befbda85e606907a013038fbd1
sachityag <sachityagi0807@gmail.com>
2023-02-26 13:41:06 -0800

    game can now be won by diagonals

--- gameover.c
+++ gameover.c
@@ -5,7 +5,6 @@
 
 int wonbyrows(int arr[], int len)
 {
-	int i;
 	if(arr[0] != 2) {      
 		//if index 0, 1, and 2 are same
 		if(arr[0] == arr[1] && arr[0] == arr[2]) {
@@ -47,7 +46,6 @@ int wonbyrows(int arr[], int len)
 
 int wonbycolumns(int arr[], int len)
 {
-	int i;
 	if(arr[0] != 2) {      
 		//if index 0, 3, and 6 are same
 		if(arr[0] == arr[3] && arr[0] == arr[6]) {
@@ -87,11 +85,45 @@ int wonbycolumns(int arr[], int len)
 
 
 
+int wonbydiags (int arr[], int len)
+{
+	if(arr[0] != 2) {      
+		//if index 0, 4, and 8 are same
+		if(arr[0] == arr[4] && arr[0] == arr[8]) {
+			if(arr[0] == 1) {
+				printf("Player 1 has won the game!\n");
+			} else {
+				printf("Player 2 has won the game!\n");
+			}
+			return 1;
+		}
+	}
+	if(arr[2] != 2) {      
+		//if index 2, 4, and 6 are same
+		if(arr[2] == arr[4] && arr[2] == arr[6]) {
+			if(arr[2] == 1) {
+				printf("Player 1 has won the game!\n");
+			} else {
+				printf("Player 2 has won the game!\n");
+			}
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+
+
+
 int gameover(int arr[], int len) {
 	if (wonbyrows(arr, len) == 1) {
 		return 1;
 	}
 	if (wonbycolumns(arr, len) == 1) {
+		return 1;
+	}
+	if (wonbydiags(arr, len) == 1) {
 		return 1;
 	}
 	return 0;
